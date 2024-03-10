2d7fa9d19073daf89db667cf1ec09fd67ff6bf8a
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-10 06:48:51 +0500

    feat : добавил внутри формы два компонента signin signup

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,6 @@
+import { SignIn } from '../SignIn/SignIn'
+import { SignUp } from '../SignUp/SignUp.jsx'
+
 export function Form() {
 	const onSubmitSignin = (email, password) => {
 		console.log(email, password)
@@ -14,5 +17,10 @@ export function Form() {
 		console.log(name, nickname, email, gender, password, confirmPassword)
 	}
 
-	return <div></div>
+	return (
+		<div>
+			<SignIn onSubmit={onSubmitSignin} />
+			<SignUp onSubmit={onSubmitSignup} />
+		</div>
+	)
 }
