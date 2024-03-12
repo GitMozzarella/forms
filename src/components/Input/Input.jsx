88ecfbe43aab5d0f74af7915ed264403cadaa5f8
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-03-12 13:19:42 +0500

    fix: поправил разметку добавил иконку @

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,16 @@
 import { forwardRef } from 'react'
 import styles from './Input.module.css'
 
-export const Input = forwardRef((props, ref) => {
+export const Input = forwardRef(({ ...props }, ref) => {
 	const asteriskStyle = {
 		color: 'red',
 		marginLeft: '2px',
+	}
+
+	const inputStyle = {
+		borderRadius: props.radius + 'px',
+		fontSize: props.size + 'px',
+		paddingLeft: props.icon ? '40px' : '10px',
 	}
 
 	return (
@@ -20,15 +26,17 @@ export const Input = forwardRef((props, ref) => {
 				{props.required && <span style={asteriskStyle}>*</span>}
 			</label>
 			<span className={styles.description}>{props.description}</span>
-			<input
-				className={props.error ? styles.inputFieldError : styles.inputField}
-				{...props}
-				ref={ref}
-				style={{
-					borderRadius: props.radius + 'px',
-					fontSize: props.size + 'px',
-				}}
-			/>
+			<div className={styles.inputFieldContainer}>
+				{props.icon && <div className={styles.icon}>{props.icon}</div>}{' '}
+				<input
+					className={`${
+						props.error ? styles.inputFieldError : styles.inputField
+					} ${props.icon ? styles.inputWithIcon : ''}`}
+					{...props}
+					ref={ref}
+					style={inputStyle}
+				/>
+			</div>
 			<span className={styles.error}>{props.error}</span>
 		</div>
 	)
